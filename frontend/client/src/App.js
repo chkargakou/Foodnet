b912ad89cfb6e9fbb3ecc1f4bf1037c785cefe98
Chrys Kargakos <chkargakos@gmail.com>
2024-12-12 20:09:07 +0200

    added basic routing and example pages

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,92 +1,52 @@
-import axios from 'axios';
-import './App.css';
+import "./App.css";
 
-const apiCall = () => {
-  axios.get(`http://${window.location.hostname}`).then((data) => {
-    console.log(data);
-    document.getElementById('apiRes').innerText = data.data;
-  });
-};
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Products from "./components/Products";
+import About from "./components/About";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="navbar bg-base-100">
-          <div className="navbar-start">
-            <details className="dropdown">
-              <summary tabIndex={0} role="button" className="btn btn-circle">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h7" />
-                </svg>
-              </summary>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                <li><a href="/">Home</a></li>
-                <li><a href="/products">Products</a></li>
-                <li><a href="/about">About</a></li>
-              </ul>
-            </details>
-          </div>
-          <div className="navbar-center">
-            <img src="./logo.png" className="h-10 p-1" alt="Foodnet Logo" />
-            <a className="btn btn-ghost text-xl">FoodNet</a>
-          </div>
-          <div className="navbar-end">
-            <button className="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-            </button>
-            <button className="btn" onClick={() => document.getElementById('modal').showModal()}><div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-              </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
-            </div></button>
-            <dialog id="modal" className="modal">
-              <div className="modal-box">
-                <h3 className="font-bold text-lg"></h3>
-                <p id="apiRes" className="py-4">Press ESC key or click the button below to close</p>
-                <div className="modal-action">
-                    <button onClick={apiCall} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Close</button>
-                </div>
-              </div>
-            </dialog>
-          </div>
-        </div>
-        <hr />
-      </header>
-    </div>
-  );
-};
+    return (
+        <>
+            <Router>
+                <Routes>
+
+                    <Route
+                        exact
+                        path="/"
+                        element={<Home />}
+                    />
+
+                    <Route
+                        path="/products"
+                        element={<Products />}
+                    />
+
+                    <Route
+                        path="/about"
+                        element={<About />}
+                    />
+
+                    <Route
+                        path="/register"
+                        element={<Register />}
+                    />
+                    
+                    <Route
+                        path="*"
+                        element={<Navigate to="/" />}
+                    />
+                </Routes>
+            </Router>
+        </>
+    );
+}
 
 export default App;
---
+++ frontend/client/src/components/Register.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <details className="dropdown">
+              <summary tabIndex={0} role="button" className="btn btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h7" />
+                </svg>
+              </summary>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/products">Products</Link></li>
+                <li><Link to="/about">About</Link></li>
+              </ul>
+            </details>
+          </div>
+          <div className="navbar-center">
+            <Link to="/" className="btn btn-ghost text-xl"><img src="./logo.png" className="h-10 p-1" alt="Foodnet Logo" />
+              FoodNet</Link>
+          </div>
+          <div className="navbar-end">
+            <button className="btn btn-ghost" onClick={() => document.getElementById('loginModal').showModal()}>
+              Log-in/Register
+            </button>
+            <dialog id="loginModal" className="modal">
+              <div className="modal-box">
+                <h2 className="card-title text-2xl font-bold mb-6">Login</h2>
+                <form>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+                      <input type="email" className="grow" placeholder="email@example.com" />
+                    </label>
+                  </div>
+                  <div className="form-control mt-4">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+                      <input type="password" className="grow" placeholder="Enter password" />
+                    </label>
+                  </div>
+                  <div className="form-control mt-6">
+                    <button className="btn btn-primary">
+                      Login
+                    </button>
+                  </div>
+                </form>
+                <div className="divider">OR</div>
+                <div className="text-center">
+                  <p>Don't have an account?</p>
+                  <Link to="/register" className="link link-primary">Register now</Link>
+                </div>
+                <div className="modal-action">
+                  <form method="dialog">
+                    <button className="btn">Close</button>
+                  </form>
+                </div>
+              </div>
+            </dialog>
+          </div>
+        </div>
+        <hr />
+      </header>
+      register
+    </div>
+  );
+};
+
+export default App;
