fed754d01e747f8db0c1f6c4a174c42fdedcbc5e
Chrys Kargakos <chkargakos@gmail.com>
2024-12-14 08:25:27 +0200

    send register info to backend and minor changes

--- frontend/client/src/components/Register.jsx
+++ frontend/client/src/components/Register.jsx
@@ -1,5 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
+
+const apiCall = async (e) => {
+  e.preventDefault();
+  let username = document.getElementsByName("username")[0].value;
+  let password = document.getElementsByName("password")[0].value;
+  let confirmPass = document.getElementsByName("passConfirm")[0].value;
+
+  const delay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
+
+  await axios.post(`http://${window.location.hostname}/register`,
+    {
+      username: username,
+      password: password,
+      confirmPass: confirmPass,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(async (response) => {
+      let res = response.data;
+
+      if (res === "badPass") {
+        console.log("ok")
+        document.getElementById('passErr').classList.remove("hidden");
+        document.getElementById('passErr').classList.add("err-visible");
+        document.getElementById('passErr').classList.remove("err-hidden");
+        await delay(2000);
+        // fade out
+        document.getElementById('passErr').classList.add("err-hidden");
+        document.getElementById('passErr').classList.remove("err-visible");
+        return;
+      }
+
+      console.log(res);
+    });
+};
 
 function App() {
   return (
@@ -83,7 +124,56 @@ function App() {
         </div>
         <hr />
       </header>
-      register
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Username</span>
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+              <input name="username" type="text" className="grow" placeholder="Username" required />
+            </label>
+          </div>
+          <div className="form-control mt-4">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+              <input name="password" type="password" className="grow" placeholder="Enter password" required />
+            </label>
+          </div>
+          <div className="form-control mt-4">
+            <label className="label">
+              <span className="label-text">Confirm Password</span>
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+              <input name="passConfirm" type="password" className="grow" placeholder="Enter password" required />
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button onClick={apiCall} className="btn btn-primary">
+              Register
+            </button>
+          </div>
+          <div id="passErr" role="alert" className="py-2 my-8 hidden alert alert-error">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 shrink-0 stroke-current"
+              fill="none"
+              viewBox="0 0 24 24">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span>Passwords do not match</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
